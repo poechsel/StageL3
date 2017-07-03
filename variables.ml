0cdb3e4645f39b5179dfb967176f8356491f9bb6
Oechsel Pierre <pierre.oechsel@ens-lyon.fr>
2017-07-03 17:10:18 +0200

    refactored things

--- variables.ml
+++ variables.ml
@@ -65,54 +65,6 @@ let unop_pure_for_loop op i =
 let one = Constant(CInt(Dec, Num.num_of_int 1, ""))
 let uuid_iterateur = ref 0
 
-(* return a triplet (i, start, end, step) if we have a pure for loop.
-   i is an iterator going from start to end (included) by a step of step
-   *)
-let create_iterateur for_loop =
-  match for_loop with
-  | For(Some start, Some end_cond, Some it, _) ->
-    let var_name, start = match start with
-      | Declaration(_, [(name, _), _, _, Some start]) -> name, start
-      | Assign(BinOp.Empty, Identifier(name, uuid), start) -> name, start
-      | _ -> failwith "start indices bad formatted"
-    in let stop = match end_cond with
-        | Identifier(x, uuid) when x = var_name -> Constant(CInt(Dec, Num.num_of_int 0, ""))
-        (* i < n *)
-        | BinaryOp(BinOp.Slt, Identifier(r, uuid), l) when r = var_name -> 
-          BinaryOp (BinOp.Sub, l, one)
-        (* n < i *)
-        | BinaryOp(BinOp.Slt, r, Identifier(l, uuid)) when l = var_name -> 
-          BinaryOp (BinOp.Add, r, one)
-        (* i > n *)
-        | BinaryOp(BinOp.Sgt, Identifier(r, uuid), l) when r = var_name -> 
-          BinaryOp (BinOp.Add, l, one)
-        (* n > i *)
-        | BinaryOp(BinOp.Sgt, r, Identifier(l, uuid)) when l = var_name -> 
-          BinaryOp (BinOp.Sub, r, one)
-            (* <= and => *)
-        | BinaryOp(BinOp.Leq, r, Identifier(l, uuid)) 
-        | BinaryOp(BinOp.Leq, Identifier(l, uuid), r)  
-        | BinaryOp(BinOp.Geq, r, Identifier(l, uuid))  
-        | BinaryOp(BinOp.Geq, Identifier(l, uuid), r) when l = var_name -> 
-          r
-        | _ -> let _ = Printf.printf "========ERROR ===========\n%s\n" (pretty_print_ast end_cond) in failwith "not a good stop condition"
-
-    in let step = match it with 
-        | UnaryOp(UnOp.PostIncr, Identifier(x, uuid)) 
-        | UnaryOp(UnOp.PreIncr, Identifier(x, uuid)) when x = var_name ->
-          (BinOp.Add, one)
-        | UnaryOp(UnOp.PostDecr, Identifier(x, uuid)) 
-        | UnaryOp(UnOp.PreDecr, Identifier(x, uuid)) when x = var_name ->
-          (BinOp.Sub, one)
-        | Assign(op, Identifier(x, uuid), b) when x = var_name ->
-          (op, b)
-        | _ -> failwith "wrong step"
-    in let temp = (var_name, !uuid_iterateur, start, stop, step)
-    in let _ = incr uuid_iterateur
-    in temp
-
-  | _ -> raise Not_found
-
 
 let pretty_print_iterator it = 
   let (var_name, _, start, stop, (_, step)) = it
@@ -155,9 +107,9 @@ let add_variable tbl name forloop indices uuids level permission =
       Hashtbl.add tbl name (level, [permission, forloop, indices, uuids])
 
 
-let get_all_variables program = 
-  let tbl = Hashtbl.create 0 in
-  let rec aux forloop_list indices_list uuids level permission program =
+
+  let rec aux tbl forloop_list indices_list uuids level permission program =
+    let aux = aux tbl in
   match program with
     | Identifier (";", _) | Identifier ("", _)  -> ()
     | Identifier(s, uuid) -> 
@@ -239,8 +191,68 @@ let get_all_variables program =
 
 
 
-  in let _ = List.iter (fun x -> aux [] [] [] 0 0 x) program 
+
+
+
+(* return a triplet (i, start, end, step) if we have a pure for loop.
+   i is an iterator going from start to end (included) by a step of step
+   *)
+and create_iterateur for_loop =
+  match for_loop with
+  | For(Some start, Some end_cond, Some it, stmts) ->
+    let var_name, start = match start with
+      | Declaration(_, [(name, _), _, _, Some start]) -> name, start
+      | Assign(BinOp.Empty, Identifier(name, uuid), start) -> name, start
+      | _ -> failwith "start indices bad formatted"
+    in let stop = match end_cond with
+        | Identifier(x, uuid) when x = var_name -> Constant(CInt(Dec, Num.num_of_int 0, ""))
+        (* i < n *)
+        | BinaryOp(BinOp.Slt, Identifier(r, uuid), l) when r = var_name -> 
+          BinaryOp (BinOp.Sub, l, one)
+        (* n < i *)
+        | BinaryOp(BinOp.Slt, r, Identifier(l, uuid)) when l = var_name -> 
+          BinaryOp (BinOp.Add, r, one)
+        (* i > n *)
+        | BinaryOp(BinOp.Sgt, Identifier(r, uuid), l) when r = var_name -> 
+          BinaryOp (BinOp.Add, l, one)
+        (* n > i *)
+        | BinaryOp(BinOp.Sgt, r, Identifier(l, uuid)) when l = var_name -> 
+          BinaryOp (BinOp.Sub, r, one)
+            (* <= and => *)
+        | BinaryOp(BinOp.Leq, r, Identifier(l, uuid)) 
+        | BinaryOp(BinOp.Leq, Identifier(l, uuid), r)  
+        | BinaryOp(BinOp.Geq, r, Identifier(l, uuid))  
+        | BinaryOp(BinOp.Geq, Identifier(l, uuid), r) when l = var_name -> 
+          r
+        | _ -> let _ = Printf.printf "========ERROR ===========\n%s\n" (pretty_print_ast end_cond) in failwith "not a good stop condition"
+
+    in let step = match it with 
+        | UnaryOp(UnOp.PostIncr, Identifier(x, uuid)) 
+        | UnaryOp(UnOp.PreIncr, Identifier(x, uuid)) when x = var_name ->
+          (BinOp.Add, one)
+        | UnaryOp(UnOp.PostDecr, Identifier(x, uuid)) 
+        | UnaryOp(UnOp.PreDecr, Identifier(x, uuid)) when x = var_name ->
+          (BinOp.Sub, one)
+        | Assign(op, Identifier(x, uuid), b) when x = var_name ->
+          (op, b)
+        | _ -> failwith "wrong step"
+    in let temp = (var_name, !uuid_iterateur, start, stop, step)
+    in let _ = incr uuid_iterateur
+    in let _ = 
+         let r = get_all_variables ([stmts]) in ()
+    in temp
+
+  | _ -> raise Not_found
+
+
+
+and get_all_variables program = 
+  let tbl = Hashtbl.create 0 in
+
+
+   let _ = List.iter (fun x -> aux tbl [] [] [] 0 0 x) program 
   in tbl
+
 
 
 (* replace all identifier having an id in ids by expr *)
